c8d7be64085ef27f0b7e35f7f3bc5b00b8cae624
Nedim Džibrić <47319999+Nedimm23@users.noreply.github.com>
2019-08-22 12:53:19 +0200

    almost finished aplication

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) { }
+  constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) { }
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -18,16 +19,17 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    return this.http.get<Recipe[]>('https://recipeapp-project-8f9a5.firebaseio.com/recipes.json')
-      .pipe(map(recipes => {
+    return this.http.get<Recipe[]>('https://recipeapp-project-8f9a5.firebaseio.com/recipes.json'
+    ).pipe(
+      map(recipes => {
         return recipes.map(recipe => {
           return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] };
         });
       }),
-        tap(recipes => {
-          this.recipeService.setRecipes(recipes);
-        })
-      );
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    );
   }
 
 }
